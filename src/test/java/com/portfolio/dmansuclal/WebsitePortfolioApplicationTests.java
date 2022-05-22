7ae98f06c10949c8bce008506e9d7db97fefa5c0
dm470 <dm470@student.le.ac.uk>
2022-05-22 16:47:33 +0100

    Initial commit

---
+++ src/test/java/com/portfolio/dmansuclal/WebsitePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dmansuclal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsitePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
